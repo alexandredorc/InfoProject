0ee14058876e436cad84415a8b707df78e0ed0bd
William Nguyen <william.nguyen@hotmail.fr>
2021-03-30 09:39:36 +0200

    allocation mémoire plateau

--- headers/plateau.h
+++ headers/plateau.h
@@ -1,7 +1,17 @@
-struct plateau{
-    struct tuiles grille[7][7];
-    struct tuiles solo;
-    bool mobile[7][7]; 
+#ifndef _PLATEAU_
+#define _PLATEAU_
 
+struct plateau{
+    int taille; 
+    struct tuiles **grille;
+    struct tuiles *solo;
+    bool *ligne_mobile;
+    bool *colonne_mobile;
 };
+
 typedef struct plateau plateau;
+
+plateau* initplat_alloc(const int taille);
+void free_plat(plateau* p);
+
+#endif
--- plateau.c
+++ plateau.c
@@ -1,38 +1,79 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "tuiles.h"
+#include "headers/tuiles.h"
+#include "headers/plateau.h"
 
-struct plateau{
-    struct tuiles grille[7][7];
-    struct tuiles solo;
-    bool mobile[7][7]; 
+plateau* initplat_alloc(const int taille)
+{
+    plateau *res = malloc(sizeof(*res));
+    res->taille=taille;
+    res->grille = malloc(taille*sizeof(*res->grille));
+    for(int i=0; i<taille;i++){
+        res->grille[i] = malloc(taille*sizeof(*res->grille[i]));
+    }
+    res->ligne_mobile = malloc(taille*sizeof(bool));
+    res->colonne_mobile = malloc(taille*sizeof(bool));
+    res->solo = malloc(sizeof(*res->solo));
+    return res;
+}
 
-};
-
-typedef struct plateau plateau;
-
-plateau deplacementvertical(plateau p, int colonne, bool direction){
+void free_plat(plateau* p)
+{
+    free(p->solo);
+    free(p->ligne_mobile);
+    free(p->colonne_mobile);
+    for(int i=0;i<p->taille;i++){
+        free(p->grille[i]);
+    }
+    free(p->grille);
+    free(p);
+}
+/*
+plateau deplacementvertical(plateau* p, const int colonne, const bool direction){
     //direction 0=vers le bas, 1=le haut
     tuiles temp;
     if (direction==0){
-        temp=p.grille[colonne][7];
+        temp=p->grille[colonne][0];
+        
+        for (int i=0;i<6;i++){
+            p->grille[colonne][i]=p->grille[colonne][i+1];
+        }
+        p->grille[colonne][7]=p->solo;
+        p->solo=temp;
+    }
+
+    if (direction==1){
+        temp=p->grille[colonne][7];
+        
+        for (int i=0;i<6;i++){
+            p->grille[colonne][i-1]=p->grille[colonne][i];
+        }
+        p->grille[colonne][7]=p->solo;
+        p->solo=temp;
+    }
+}
+
+plateau deplacementhorizontal(plateau p, int ligne, bool direction){
+    //direction 0=vers la droite, 1=la gauche
+    tuiles temp;
+    if (direction==0){
+        temp=p.grille[7][ligne];
         
         for (int i=1;i<=7;i++){
-            p.grille[colonne][i]=p.grille[colonne][i-1];
+            p.grille[ligne][i]=p.grille[ligne][i-1];
         }
-        p.grille[colonne][0]=p.solo;
+        p.grille[ligne][0]=p.solo;
         p.solo=temp;
     }
 
     if (direction==1){
-        temp=p.grille[colonne][0];
+        temp=p.grille[ligne][0];
         
         for (int i=7;i>=1;i--){
-            p.grille[colonne][i]=p.grille[colonne][i+1];
+            p.grille[ligne][i]=p.grille[ligne][i+1];
         }
-        p.grille[colonne][7]=p.solo;
+        p.grille[ligne][7]=p.solo;
         p.solo=temp;
     }
-}
+}*/
